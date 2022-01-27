a1da9108166d54b5e15e3d1b43b049be9be8e338
Furkan <furkanvsarda@gmail.com>
2022-01-28 00:06:49 +0300

    updates

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Text, SafeAreaView, StyleSheet } from 'react-native'
+import { Text, SafeAreaView, StyleSheet, StatusBar } from 'react-native'
 import SignInScreen from "./screens/signinScreen/SignInScreen"
 import SignUpScreen from "./screens/signupScreen/SignUpScreen"
 
 export default function Router() {
     return (
-        <SignUpScreen />
+        <StatusBar barStyle='light-content' >
+            <SignUpScreen />
+        </StatusBar>
     )
 }
--- src/screens/signupScreen/SignUpScreen.js
+++ src/screens/signupScreen/SignUpScreen.js
@@ -29,7 +29,6 @@ export default function SignUpScreen() {
             </View>
         </SafeAreaView>
     );
-
 }
 
 
